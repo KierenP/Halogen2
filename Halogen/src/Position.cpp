b6f9097600d79548418fb5f4994896a1f3c36fe3
Kieren Pearson <kierenpearson9@gmail.com>
2021-01-27 22:11:59 +1100

    Bench: 29188513

--- Halogen/src/Position.cpp
+++ Halogen/src/Position.cpp
@@ -99,7 +99,7 @@ void Position::ApplyMove(Move move)
 	UpdateCastleRights(move);
 	IncrementZobristKey(move);
 
-	if (GetBitCount(GetAllPieces()) > SPARCE_EVAL_LIMIT)
+	if (GetBitCount(GetAllPieces()) >= SPARCE_EVAL_LIMIT)
 		net.ApplyDelta(CalculateMoveDelta(move));
 
 	/*if (GenerateZobristKey() != key)
@@ -177,7 +177,7 @@ void Position::RevertMove()
 {
 	assert(PreviousKeys.size() > 0);
 
-	if (GetBitCount(GetAllPieces()) > SPARCE_EVAL_LIMIT)
+	if (GetBitCount(GetAllPieces()) >= SPARCE_EVAL_LIMIT)
 		net.ApplyInverseDelta();
 
 	RestorePreviousBoard();
